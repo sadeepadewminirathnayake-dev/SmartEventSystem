d6f4fb467fa8222b46a205b8e6c6565fe4dde3f3
Sadeepa Dewmini <sadeepadewmini683@gmai.com>
2026-02-15 22:18:00 +0530

    InquiryController

---
+++ SmartEventSystem/Controllers/InquiryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartEventSystem.Controllers
+{
+    public class InquiryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
