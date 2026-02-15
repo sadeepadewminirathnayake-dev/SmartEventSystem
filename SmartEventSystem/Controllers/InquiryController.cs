099cd3c1267148b84dd61c7160e354e6c1cbce79
Sadeepa Dewmini <sadeepadewmini683@gmai.com>
2026-02-15 22:21:43 +0530

    Create.cshtml

--- SmartEventSystem/Controllers/InquiryController.cs
+++ SmartEventSystem/Controllers/InquiryController.cs
@@ -1,12 +1,62 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using SmartEventSystem.Models;
 
 namespace SmartEventSystem.Controllers
 {
     public class InquiryController : Controller
     {
-        public IActionResult Index()
+        private readonly IConfiguration _configuration;
+
+        public InquiryController(IConfiguration configuration)
         {
-            return View();
+            _configuration = configuration;
+        }
+
+        // OPEN PAGE (Guest allowed)
+        [HttpGet]
+        public IActionResult Create(int eventId)
+        {
+            Inquiry inquiry = new Inquiry
+            {
+                EventID = eventId
+            };
+
+            return View(inquiry);
+        }
+
+        // SUBMIT INQUIRY
+        [HttpPost]
+        public IActionResult Create(Inquiry model)
+        {
+            if (ModelState.IsValid)
+            {
+                string connectionString =
+                    _configuration.GetConnectionString("SmartEventDBConnection");
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string query = @"INSERT INTO Inquiry
+                                    (EventID, GuestName, Email, Message, InquiryDate)
+                                    VALUES
+                                    (@EventID, @GuestName, @Email, @Message, GETDATE())";
+
+                    SqlCommand cmd = new SqlCommand(query, con);
+
+                    cmd.Parameters.AddWithValue("@EventID", model.EventID);
+                    cmd.Parameters.AddWithValue("@GuestName", model.GuestName);
+                    cmd.Parameters.AddWithValue("@Email", model.Email);
+                    cmd.Parameters.AddWithValue("@Message", model.Message);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+                ViewBag.Message = "Inquiry submitted successfully!";
+            }
+
+            return View(model);
         }
     }
 }
+
