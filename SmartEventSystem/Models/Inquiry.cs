c6874bcf7459df36139e9a5ba858cd1341bf3574
Sadeepa Dewmini <sadeepadewmini683@gmai.com>
2026-02-15 22:01:25 +0530

    inquiry

---
+++ SmartEventSystem/Models/Inquiry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SmartEventSystem.Models
+{
+    public class Inquiry
+    {
+        public int InquiryID { get; set; }
+        public int EventID { get; set; }
+
+        public string GuestName { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+
+        public DateTime InquiryDate { get; set; }
+    }
+}
+
